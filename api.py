f6616e7e02bd1b10445f8968f10630223b3b804e
FrankLiang <71284395+NingyueLiang@users.noreply.github.com>
2023-02-13 12:57:33 -0600

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import json
 from flask import Flask, request, jsonify, render_template
 
 api = Flask(__name__)
-chatbot = revChatGPT.Official.Chatbot(api_key = "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") 
+chatbot = revChatGPT.Official.Chatbot(api_key = "key") 
 
 @api.route('/response', methods=['POST'])
 def get_answers():
